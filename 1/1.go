886f19251aa8cb5127adff35a41b32798d153dc2
Robin Chowdhury <robinch88@gmail.com>
2014-02-20 17:59:00 +0100

    Solved the first problem

---
+++ 1/1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d", multiples_3_5(1000))
+}
+
+func multiples_3_5(n int) int {
+	total := 0
+	for i := 1; i < n; i++ {
+		if i%3 == 0 {
+			total += i
+		} else if i%5 == 0 {
+			total += i
+		}
+	}
+	return total
+}
