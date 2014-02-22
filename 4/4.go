66a2c24ccd4f6b8d1bf24b5653afdea23f249cb9
Robin Chowdhury <robinch88@gmail.com>
2014-02-22 12:38:40 +0100

    Solved problem 4

---
+++ 4/4.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(findLargestNdigitPal(3))
+}
+
+func isPalindrome(n int) bool {
+	s := strconv.Itoa(n)
+	return s == strRevers(s)
+}
+
+func strRevers(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func findLargestNdigitPal(n int) int {
+	maxRes := 0
+
+	step := 1
+	// Uses the fact that even palidromes
+	// are divisible by 11
+	if n%2 == 0 {
+		step = 11
+	}
+
+	min := 1
+	for i := 0; i < n-1; i++ {
+		min *= 10
+	}
+	max := min*10 - 1
+
+	for i := max; i >= min; i = i - step {
+		for j := max; j >= min; j = j - step {
+			if isPalindrome(i * j) {
+				if i*j > maxRes {
+					maxRes = i * j
+				}
+			}
+		}
+	}
+	return maxRes
+}
